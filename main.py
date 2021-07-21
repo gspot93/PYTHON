1b7ee30975b56e8ee7edd7dc30dc4926c157a464
Marko Giacomeli <marko.giacomeli@gmail.com>
2021-07-22 00:30:45 +0200

    Initial commit. Add python data type examples

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello! My name is Luka.")
+
+# Data types
+nothing = None
+is_life_good = True
+age = 35
+length = 100.45
+fruit = "apple 22"
+people = ["Janez",  "Mojca"]
+course = {
+    "difficulty": "beginner",
+    "number_of_sessions": 24
+}
+
+
+print(fruit + " yeah")
+print(age + 14)
